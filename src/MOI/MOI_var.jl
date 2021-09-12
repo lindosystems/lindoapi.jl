3758c6fef686aad979e8a996e06e0a1be2507787
JHaasIII92 <31041229+JHaasIII92@users.noreply.github.com>
2021-09-12 13:26:28 -0500

    Update MOI_var.jl

--- src/MOI/MOI_var.jl
+++ src/MOI/MOI_var.jl
@@ -1,9 +1,31 @@
-#
-# Add n scalar variables to the model, returning a vector of variable indices.
-#
+#=
+
+ File: MOI_var.jl
+ Brief: The functions in this file are used to crate variables
+
+ Authors: James Haas,
+
+ Bugs:
+
+ TODO: Implement MOI.add_constraint().
+       To attach a constraint to a variable JuMP calls MOI.add_constraint().
+       The data sent to this function could then be attached to the variables data
+       see _VariableInfo.
+
+=#
+
+#=
+
+ Function add_variables:
+ Breif: Add one or more variables to a model.
+
+ Param model:
+ Param N: number of variables to add.
+
+ Return indices: an array of MOI.VariableIndex
+
+=#
 function MOI.add_variables(model::Optimizer, N::Int)
-    # initilize the _VariableIndex
-    # add_item to actualy
     indices = Vector{MOI.VariableIndex}(undef, N)
     for i in 1:N
         # Initialize `_VariableInfo` with a dummy `VariableIndex` and a column,
