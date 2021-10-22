b5e29ad6252202ae0114b739416933e1a84e1fd6
JHaasIII92 <31041229+JHaasIII92@users.noreply.github.com>
2021-10-22 18:33:39 -0500

    Colville Function example

--- samples/JuMP_interface/ex_gop.jl
+++ samples/JuMP_interface/ex_gop.jl
@@ -1,5 +1,23 @@
 """
-Beale Function
+Colville Function
+
+Minimize f(x) = 100(x²₁ - x₂)² + (x₁ - 1)² + (x₃ - 1)² + 90(x²₃ - x₄)²
+       + 10.1((x₂ - 1)² + (x₄ - 1)²) + 19.8(x₂ - 1)(x₄ - 1)
+Such that: -10 ≦ xᵢ ≦ 10
+
+f(x^*) = 0, at x^* = (1, 1, 1, 1)
+
+Purpose:
+ This sample demonstrates the Lindo API's Global solver
+ as well as how that global solver using `set_optimizer_attribute`.
+
+ To run sample
+     include("/PathToUse/ex_gop.jl")
+
+To update to the most current version of Lindoapi.jl
+     Run in REPL:
+         using Pkg
+         Pkg.add(url="https://github.com/lindosystems/lindoapi.jl")
 
 """
 
@@ -12,12 +30,17 @@ using JuMP
 model = Model(Lindoapi.Optimizer)
 set_optimizer_attribute(model,"use_Global",true)
 
-@variable(model, x)
-@variable(model, y)
+@variable(model, -10 <= x[1:4] <= 10)
 
 @NLobjective(model,
              Min,
-             - (1 + cos(12*sqrt(x^2+y^2)))/(0.5*(x^2 + y^2) + 2)
+             100*(x[1]^2 - x[2])^2 + (x[1] - 1)^2 + (x[3] - 1)^2 + 90*(x[3] - x[4])^2
+              + 10.1*((x[2] - 1)^2 +(x[4] -1)^2) + 19.8*(x[2] - 1)*(x[4] - 1)
              )
 
 optimize!(model)
+
+x_star = value.(x)
+for i in 1:4
+   println("x_$(i) = ", x_star[i])
+end
