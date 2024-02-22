924502c05c8198b294324850eaaa6be0feb9fde5
Mustafa Atlihan <mkatlihan@gmail.com>
2024-02-23 01:44:41 +0300

    licfile

--- samples/soft_wrapper/mip.jl
+++ samples/soft_wrapper/mip.jl
@@ -62,7 +62,7 @@ pachVarType = Cchar['B','B','B','B','B','B']
 
 # Get API key
 LicenseKey = Vector{UInt8}(undef,1024)                                          # Allocating memory for a vector of 1024 UInt8
-license_path = joinpath(PATH,"license/lndapi130.lic")                           # Creating a license path to where the key is
+license_path = joinpath(PATH,"license/lndapi140.lic")                           # Creating a license path to where the key is
 ret = LS.LSloadLicenseString(license_path, LicenseKey)                          # Now calling API function
 if ret != 0
     error("Key not found check key $(license_path)")                            # Throwing an error if key not found
